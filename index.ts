3757e61009bbed856c6726a5c3160a5c5837ab01
Tatsuya Yamamoto <the.kinnikuman@gmail.com>
2020-09-20 17:49:03 +0000

    init

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { createServer } from 'http';
+
+createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(3000, '127.0.0.1');
