ce0859179f2902d4c7b1778f85702c2cc114164e
Tatsuya Yamamoto <the.kinnikuman@gmail.com>
2020-09-20 18:24:46 +0000

    hoge

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const port = "3001";
+const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
